5d70be5bb0a7134f835c37b0659a7e8d13b9bc01
nagesh014 <970459tn@gmail.com>
2026-09-11 23:54:59 +0530

    Add files via upload

---
+++ pizza_sales.db[1].sql
@@ -0,0 +1,55 @@
+SELECT * from pizza;
+
+-- Total Revenue:
+SELECT SUM(total_price) AS Total_Revenue FROM pizza;
+ 
+-- Average Order Value
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_order_Value FROM pizza;
+ 
+-- Total Pizzas Sold
+SELECT SUM(quantity) AS Total_pizza_sold FROM pizza;
+ 
+-- Total Orders
+SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza;
+ 
+-- Average Pizzas Per Order
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+AS Avg_Pizzas_per_order
+FROM pizza;
+ 
+-- % of Sales by Pizza Category
+SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza) AS DECIMAL(10,2)) AS PCT
+FROM pizza
+GROUP BY pizza_category;
+ 
+-- % of Sales by Pizza Size
+SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
+CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza) AS DECIMAL(10,2)) AS PCT
+FROM pizza
+GROUP BY pizza_size
+ORDER BY pizza_size;
+ 
+-- Total Pizzas Sold by Pizza Category
+SELECT pizza_category, SUM(quantity) as Total_Quantity_Sold
+FROM pizza
+WHERE MONTH(order_date) = 2
+GROUP BY pizza_category
+ORDER BY Total_Quantity_Sold DESC;
+ 
+-- Top 5 Best Sellers by Total Pizzas Sold
+SELECT pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold DESC
+LIMIT 5;
+
+ -- Bottom 5 Best Sellers by Total Pizzas Sold
+SELECT  pizza_name, SUM(quantity) AS Total_Pizza_Sold
+FROM pizza
+GROUP BY pizza_name
+ORDER BY Total_Pizza_Sold ASC 
+LIMIT 5;
+
+
